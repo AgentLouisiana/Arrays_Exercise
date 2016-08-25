29095691a1e4ce0c712c8e7384a76d74b1b66dbe
Darien LeLeux <austinblackofficial@gmail.com>
2016-08-25 15:12:32 -0500

    Number 6 Commit
    Worked on problem number 6.

--- Arrays_Exercise/Source.cpp
+++ Arrays_Exercise/Source.cpp
@@ -3,217 +3,206 @@
 int main()
 
 {
-	//{
-	//	//#1
+		////#1
 
-	//	/*int numbers[10] = {0, 0, 1, 0, 0, 1, 0, 0, 1, 1 }; -- Valid
-	//	int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 }; -- Invalid. It has too many assigned numbers
-	//	double radii[10] = {3.2, 4.7}; -- Invalid - You cannot have decimals
-	//	int table[7] = {2, 0, 0, 27, , 45, 39} - Invalid. An arry cannot have missing values
-	//	char codes[] = { 'A', 'X', '1', '2', 's'| }; - Valid
-	//	int blanks[]; - Invalid. It has no identifyer
-	//	int collection[-20]; - Invalid. You cannot have a negative number
-	//	int hours[3] = 8, 12, 16; - Invalid. It does not have any "{}"*/
+		///*int numbers[10] = {0, 0, 1, 0, 0, 1, 0, 0, 1, 1 }; -- Valid
+		//int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 }; -- Invalid. It has too many assigned numbers
+		//double radii[10] = {3.2, 4.7}; -- Invalid - You cannot have decimals
+		//int table[7] = {2, 0, 0, 27, , 45, 39} - Invalid. An arry cannot have missing values
+		//char codes[] = { 'A', 'X', '1', '2', 's'| }; - Valid
+		//int blanks[]; - Invalid. It has no identifyer
+		//int collection[-20]; - Invalid. You cannot have a negative number
+		//int hours[3] = 8, 12, 16; - Invalid. It does not have any "{}"*/
 
-	//	int number[10] = { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 };
+		//int number[10] = { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 };
 
-	//	/*int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 };*/
+		////int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 };
 
-	//	/*double radii[10] = { 3.2, 4.7 };*/
+		////double radii[10] = { 3.2, 4.7 };
 
-	//	/*int table[7] = { 2, , , 27, , 45, 39 };*/
+		////int table[7] = { 2, , , 27, , 45, 39 };
 
-	//	/*char codes[] = { 'A', 'X', '1', '2', 's' | };*/
+		////char codes[] = { 'A', 'X', '1', '2', 's' | };
 
-	//	/*int blanks[];*/
+		////int blanks[];
 
-	//	/*int collection[-20];*/
+		////int collection[-20];
 
-	//	/*int hours[3] = 8, 12, 16;*/
+		////int hours[3] = 8, 12, 16;
 
-	//	system("pause");
-	//}
-	///*{*/
-	//	#2
+		//system("pause");
 
-	//	int values[] = { 2, 6, 10, 14 };
-	//	std::cout << values[2];
+		////#2
 
-	//	system("pause");
+		//int values[] = { 2, 6, 10, 14 };
+		//std::cout << values[2];
 
-	//	/*int values[] = { 2, 6, 10, 14 };
-	//	std::cout << ++values[0];
+		//system("pause");
 
-	//	system("pause");
+		//int values[] = { 2, 6, 10, 14 };
+		//std::cout << ++values[0];
 
-	//	int values[] = { 2, 6, 10, 14 };
-	//	std::cout << values[1]++;
+		//system("pause");
 
-	//	system("pause");
+		//int values[] = { 2, 6, 10, 14 };
+		//std::cout << values[1]++;
 
-	//	int values[] = { 2, 6, 10, 14 };
-	//	int x = 2;
-	//	std::cout << values[++x];
+		//system("pause");
 
-	//	system("pause");
+		//int values[] = { 2, 6, 10, 14 };
+		//int x = 2;
+		//std::cout << values[++x];
 
-	//	int values[] = { 2, 6, 10, 14 };
-	//	std::cout << values[4];
+		//system("pause");
 
-	//	system("pause");*/
-	//}
-	///*{*/
-	//	#3
-	//	
-	//	/*int data[10];
-	//	for (int data = 10; data > 0; data--)
-	//		std::cout << data << std::endl;
-	//}*/
-	//{
-	//	//4
+		//int values[] = { 2, 6, 10, 14 };
+		//std::cout << values[4];
 
-	//	std::cout << "Give me 5 numbers" << std::endl;
+		//system("pause");
 
-	//	int numb[5];
-	//	std::cin >> numb[0];
-	//	std::cin >> numb[1];
-	//	std::cin >> numb[2];
-	//	std::cin >> numb[3];
-	//	std::cin >> numb[4];
+		////#3
+		//
+		//int data[10];
+		//for (int data = 10; data > 0; data--)
+		//	std::cout << data << std::endl;
+		////4
 
-	//	for (int i = 4; i >= 0; i--)
-	//	{
-	//		std::cout << numb[i] << std::endl;
-	//	}
-	//}
-	//{
-	//	//#5
+		//std::cout << "Give me 5 numbers" << std::endl;
 
-	//	std::cout << "Give me 10 numbers" << std::endl;
-	//	
-	//	int numb[10];
-	//	int i = 0;
+		//int numb[5];
+		//std::cin >> numb[0];
+		//std::cin >> numb[1];
+		//std::cin >> numb[2];
+		//std::cin >> numb[3];
+		//std::cin >> numb[4];
 
-	//	std::cin >> numb[0];
-	//	std::cin >> numb[1];
-	//	std::cin >> numb[2];
-	//	std::cin >> numb[3];
-	//	std::cin >> numb[4];
-	//	std::cin >> numb[5];
-	//	std::cin >> numb[6];
-	//	std::cin >> numb[7];
-	//	std::cin >> numb[8];
-	//	std::cin >> numb[9];
+		//for (int i = 4; i >= 0; i--)
+		//{
+		//	std::cout << numb[i] << std::endl;
+		//}
+		////#5
 
-	//	int largenum = numb[0];
-	//	int smallnum = numb[0];
+		//std::cout << "Give me 10 numbers" << std::endl;
+		//
+		//int numb[10];
+		//int i = 0;
 
-	//	for (i = 0; i < 9; i--);
-	//	{
-	//		if (largenum < numb[0])
-	//		{
-	//			largenum = numb[0];
-	//		}
-	//		if (smallnum > numb[0])
-	//		{
-	//			smallnum = numb[0];
-	//		}
-	//		if (largenum < numb[1])
-	//		{
-	//			largenum = numb[1];
-	//		}
-	//		if (smallnum > numb[1])
-	//		{
-	//			smallnum = numb[1];
-	//		}
-	//		if (largenum < numb[2])
-	//		{
-	//			largenum = numb[2];
-	//		}
-	//		if (smallnum > numb[2])
-	//		{
-	//			smallnum = numb[2];
-	//		}
-	//		if (largenum < numb[3])
-	//		{
-	//			largenum = numb[3];
-	//		}
-	//		if (smallnum > numb[3])
-	//		{
-	//			smallnum = numb[3];
-	//		}
-	//		if (largenum < numb[4])
-	//		{
-	//			largenum = numb[4];
-	//		}
-	//		if (smallnum > numb[4])
-	//		{
-	//			smallnum = numb[4];
-	//		}
-	//		if (largenum < numb[5])
-	//		{
-	//			largenum = numb[5];
-	//		}
-	//		if (smallnum > numb[5])
-	//		{
-	//			smallnum = numb[5];
-	//		}
-	//		if (largenum < numb[6])
-	//		{
-	//			largenum = numb[6];
-	//		}
-	//		if (smallnum > numb[6])
-	//		{
-	//			smallnum = numb[6];
-	//		}
-	//		if (largenum < numb[7])
-	//		{
-	//			largenum = numb[7];
-	//		}
-	//		if (smallnum > numb[7])
-	//		{
-	//			smallnum = numb[7];
-	//		}
-	//		if (largenum < numb[8])
-	//		{
-	//			largenum = numb[8];
-	//		}
-	//		if (smallnum > numb[8])
-	//		{
-	//			smallnum = numb[8];
-	//		}
-	//		if (largenum < numb[9])
-	//		{
-	//			largenum = numb[9];
-	//		}
-	//		if (smallnum > numb[9])
-	//		{
-	//			smallnum = numb[9];
-	//		}
+		//std::cin >> numb[0];
+		//std::cin >> numb[1];
+		//std::cin >> numb[2];
+		//std::cin >> numb[3];
+		//std::cin >> numb[4];
+		//std::cin >> numb[5];
+		//std::cin >> numb[6];
+		//std::cin >> numb[7];
+		//std::cin >> numb[8];
+		//std::cin >> numb[9];
 
-	//		std::cout << largenum << smallnum;
-	//	}
-	//}
-	{
+		//int largenum = numb[0];
+		//int smallnum = numb[0];
+
+		//for (i = 0; i < 9; i--);
+		//{
+		//	if (largenum < numb[0])
+		//	{
+		//		largenum = numb[0];
+		//	}
+		//	if (smallnum > numb[0])
+		//	{
+		//		smallnum = numb[0];
+		//	}
+		//	if (largenum < numb[1])
+		//	{
+		//		largenum = numb[1];
+		//	}
+		//	if (smallnum > numb[1])
+		//	{
+		//		smallnum = numb[1];
+		//	}
+		//	if (largenum < numb[2])
+		//	{
+		//		largenum = numb[2];
+		//	}
+		//	if (smallnum > numb[2])
+		//	{
+		//		smallnum = numb[2];
+		//	}
+		//	if (largenum < numb[3])
+		//	{
+		//		largenum = numb[3];
+		//	}
+		//	if (smallnum > numb[3])
+		//	{
+		//		smallnum = numb[3];
+		//	}
+		//	if (largenum < numb[4])
+		//	{
+		//		largenum = numb[4];
+		//	}
+		//	if (smallnum > numb[4])
+		//	{
+		//		smallnum = numb[4];
+		//	}
+		//	if (largenum < numb[5])
+		//	{
+		//		largenum = numb[5];
+		//	}
+		//	if (smallnum > numb[5])
+		//	{
+		//		smallnum = numb[5];
+		//	}
+		//	if (largenum < numb[6])
+		//	{
+		//		largenum = numb[6];
+		//	}
+		//	if (smallnum > numb[6])
+		//	{
+		//		smallnum = numb[6];
+		//	}
+		//	if (largenum < numb[7])
+		//	{
+		//		largenum = numb[7];
+		//	}
+		//	if (smallnum > numb[7])
+		//	{
+		//		smallnum = numb[7];
+		//	}
+		//	if (largenum < numb[8])
+		//	{
+		//		largenum = numb[8];
+		//	}
+		//	if (smallnum > numb[8])
+		//	{
+		//		smallnum = numb[8];
+		//	}
+		//	if (largenum < numb[9])
+		//	{
+		//		largenum = numb[9];
+		//	}
+		//	if (smallnum > numb[9])
+		//	{
+		//		smallnum = numb[9];
+		//	}
+
+		//	std::cout << largenum << smallnum;
+		//}
 		//#6
 
+	int map[3][3];
 
-	}
-	{
+		for (int row = 0; row < 3; ++row)
+		{
+			for (int col = 0; col < 3; ++col)
+			{
+				std::cout << map[3][3];
+			}
+		}
+
+		std::cout << map << std::endl;
+
 		//#7
-
-
-	}
-	{
 		//#8
-
-
-	}
-	{
 		//#9
-
-
-	}
 	system("pause");
 	return 0;
 }
